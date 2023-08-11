cefe953e86ffcfdf5ef6e4853c66a297c7b2d035
Yi-Fei Lim <YLim@aucotec.com>
2023-08-12 00:26:59 +0200

    Add start script

--- app.js
+++ app.js
@@ -160,6 +160,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3100, () => {
-    console.log('Serving on port 3100')
+const port = process.env.PORT || 3100;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
